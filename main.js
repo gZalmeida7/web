c5a685fbfe0c4c5adc8d55ac14fd45287397e4ae
gZalmeida7 <davi.almeida.paula@escola.pr.gov.br>
2026-07-06 11:30:55 -0300

    a

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 const botoes = document.querySelectorAll("button")
 console.log(botoes)
 
-botoes.forEach(function (botao){
-let curtu =false
-botao.addEventListener("click",botaoclicado)
-function botaoclicado
-console.log ('botaoclicado')
+botoes.forEach(function (botao){{
+    console.log ('botao clicado')}
+    let texto =botao.querySelector("span")
+    if (curtiu= =false) {
+        texto.textContent++
+        
+    }
+
+
 
 
 })
