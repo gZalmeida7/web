c67ab77f61578738b9aa49c69cb8d121b8839b14
gZalmeida7 <davi.almeida.paula@escola.pr.gov.br>
2026-07-06 11:21:09 -0300

    a

--- main.js
+++ main.js
@@ -2,5 +2,10 @@ const botoes = document.querySelectorAll("button")
 console.log(botoes)
 
 botoes.forEach(function (botao){
-
+let curtu =false
+botao.addEventListener("click",botaoclicado)
+function botaoclicado
+console.log ('botaoclicado'){
+    
+}
 })
